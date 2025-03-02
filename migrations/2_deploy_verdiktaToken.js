863f7630c1b4f2a19984e29b0db2492efa39d0c3
yenachar <yenachar@protonmail.com>
2025-03-01 23:01:46 -0500

    Added printing the network in preparation for moving networks.

--- migrations/2_deploy_verdiktaToken.js
+++ migrations/2_deploy_verdiktaToken.js
@@ -4,5 +4,6 @@ module.exports = async function(deployer, network) {
   await deployer.deploy(VerdiktaToken);
   const verdiktaToken = await VerdiktaToken.deployed();
   console.log("VerdiktaToken deployed at:", verdiktaToken.address);
+  console.log(`On network: ${network}`);
 };
 
