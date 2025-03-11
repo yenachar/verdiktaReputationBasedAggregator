011fb0df9ab378e2141b16b34a9c62f1ad7d47bd
yenachar <yenachar@protonmail.com>
2025-03-10 23:44:50 -0400

    Comment change.

--- scripts/check-address.js
+++ scripts/check-address.js
@@ -1,4 +1,5 @@
 // untility for capitalizing address for checksum
+// to run: node checksum.js
 const { utils } = require("ethers");
 const rawAddress = "0x8e5e40f8f9103168c7d7cf361c6c0fcbcb8b9b2b"; // Change this. Note all lowercase.
 const checksummedAddress = utils.getAddress(rawAddress);
