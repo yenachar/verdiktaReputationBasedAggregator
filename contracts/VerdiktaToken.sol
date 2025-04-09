f36d12eb944d7172e6a6a9f7acdda0b25efa81d5
yenachar <yenachar@protonmail.com>
2025-04-09 18:21:24 -0400

    Changed from 10M to 20M total tokens.

--- contracts/VerdiktaToken.sol
+++ contracts/VerdiktaToken.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract VerdiktaToken is ERC20, Ownable {
     constructor() ERC20("Verdikta", "VDKA") Ownable(msg.sender) {
         // Initial supply minted to owner
-        _mint(msg.sender, 10000000 * 10 ** decimals());
+        _mint(msg.sender, 20000000 * 10 ** decimals());
     }
     
     // Only owner can mint new tokens
