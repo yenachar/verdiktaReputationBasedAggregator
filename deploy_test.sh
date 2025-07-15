aaea508280ae6c10bd0bf79e67fda77e40a8fae9
yenachar <yenachar@protonmail.com>
2025-07-14 22:08:56 -0400

    Cleanup to match current deployment of token contracts only.

--- deploy_test.sh
+++ deploy_test.sh
@@ -7,6 +7,6 @@ echo "Running migrations 1-2 on Sepolia..."
 MIGRATE_ERC20=1 truffle migrate -f 1 --to 2 --network sepolia
 # echo "Running migrations 1 and 3-5 on Base Sepolia..."
 # truffle migrate -f 1 --to 1 --network base_sepolia
-echo "Running migrations 3-5 on Sepolia..."
-MIGRATE_ERC20=1 truffle migrate -f 3 --to 5 --network base_sepolia
+echo "Running migration 3 on Sepolia..."
+MIGRATE_ERC20=1 truffle migrate -f 3 --to 3 --network base_sepolia
 # truffle migrate -f 3 --to 3 --network base_sepolia
