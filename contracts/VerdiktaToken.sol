82ddf8ac9d52636e65577dd80fc2aa57552368d4
yenachar <yenachar@protonmail.com>
2025-07-14 22:58:00 -0400

    Removed mint. Removed Ownable. Improved syntax on initial supply.

--- contracts/VerdiktaToken.sol
+++ contracts/VerdiktaToken.sol
@@ -2,17 +2,11 @@
 pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VerdiktaToken is ERC20, Ownable {
-    constructor() ERC20("Verdikta", "VDKA") Ownable(msg.sender) {
-        // Initial supply minted to owner
-        _mint(msg.sender, 20000000 * 10 ** decimals());
-    }
-    
-    // Only owner can mint new tokens
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+contract VerdiktaToken is ERC20 {
+    constructor() ERC20("Verdikta", "VDKA") {
+        // Initial supply 
+        _mint(msg.sender, 20_000_000 * 1e18);
     }
     
     // Allow token holders to burn their own tokens
